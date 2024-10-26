dbd0dc787835a81575642348a592ef468b5e5a35
MianSaadTahir <tahirsaad99@gmail.com>
2024-10-26 15:31:09 +0500

    added selection sort

---
+++ Sorting/selectionSort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void selectionSort(vector<int> &arr);
+
+int main()
+{
+    vector<int> arr = {4, 2, 8, 9, 1};
+    selectionSort(arr);
+
+    cout << "Selection Sorted Array: \n";
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
+
+void selectionSort(vector<int> &arr)
+{
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++) // last element will be sorted after first pass so no need to go to last element
+    {
+        int min_index = i;              // by default take each i as minimum value index
+        for (int j = i + 1; j < n; j++) // start ahead of i
+        {
+            if (arr[j] < arr[min_index])
+            {
+                min_index = j; // keep updating minimum index to the smallest element found at j
+            }
+        }
+        if (min_index != i) // swap only if minimum index has been updated meaning that i is not the minimum index anymore. just to avoid unnecessary swaps
+        {
+            swap(arr[i], arr[min_index]);
+        }
+    }
+}
