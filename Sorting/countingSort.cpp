7d73da1c3c8a50f978b20fa1541b31cd6dcf40ef
MianSaadTahir <tahirsaad99@gmail.com>
2024-10-26 18:59:58 +0500

    updated counting comments

--- Sorting/countingSort.cpp
+++ Sorting/countingSort.cpp
@@ -21,7 +21,7 @@ int main()
 
 void countingSort(vector<int> &input)
 {
-    int n = input.size(); // size of input vec
+    int n = input.size(); // size of input vector
     if (n == 0)
     { // if empty input
         return;
@@ -29,13 +29,13 @@ void countingSort(vector<int> &input)
     int m = 0; // declare max value
     for (int i = 0; i < n; i++)
     {
-        m = max(m, input[i]); // find max value in input and assign to m
+        m = max(m, input[i]); // find max value in input vector and assign to m
     }
-    vector<int> count(m + 1, 0); // count vec of 0 elements of  size max+1
-    vector<int> output(n, 0);    // output vec of 0 elements of  size equal to length of input vec
+    vector<int> count(m + 1, 0); // count vector of 0 elements of  size max+1
+    vector<int> output(n, 0);    // output vector of 0 elements of  size equal to length of input vec
 
     for (int i = 0; i < n; i++)
-    { // counting occurences from input vec and incrementing relevant count index
+    { // counting occurences of each element in input vector and incrementing relevant count index
         count[input[i]]++;
     }
     // calculate cummulative counts of count vector
@@ -43,11 +43,11 @@ void countingSort(vector<int> &input)
     {
         count[i] += count[i - 1];
     }
-    // populate output vector by traversing input vec in reverse direction for stable sort
+    // populate output vector by traversing input vector in reverse direction for stable sort
     for (int i = n - 1; i >= 0; i--)
     {
         output[count[input[i]] - 1] = input[i];
-        count[input[i]]--; // decrease count from input element index to maintain stability
+        count[input[i]]--; // decrement count to maintain stability and make sure that duplicates are placed in order
     }
     // copy output elements to original input vector for inplace sorting
     for (int i = 0; i < n; i++)
