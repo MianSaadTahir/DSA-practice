32d6dd63bc60e651aedf559e07cd2a953613c60a
MianSaadTahir <tahirsaad99@gmail.com>
2024-10-27 09:59:21 +0500

    added time to append

---
+++ Lists/timeTOappend.py
@@ -0,0 +1,11 @@
+import time
+
+numbers = []
+
+for i in range(1, 101):
+    start = time.time()
+    numbers.append(i)
+    end = time.time()
+    append_time = end-start
+    # convert appending time to float
+    print(f"Appending {i} took {append_time:.10f}seconds")
